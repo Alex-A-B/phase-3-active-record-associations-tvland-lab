905605a55e5449156e03949d070cf4827c1b2d81
Alex-A-B <alexjamesbelton@gmail.com>
2021-11-05 11:10:39 +0000

    commit tree f4d34db49439595398f7354f7b3d16f5c8028c96

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
+  has_many :actors, through: :characters
   belongs_to :network
-  has_many :actors, through: :character
 
     def actors_list
         self.actors.map do |actor|
